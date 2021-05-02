e47718b110f1779fd0c25e7a73d98b29c7493c9f
Curtis <c.b.punches@gmail.com>
2021-05-02 19:06:04 -0400

    mountains

---
+++ src/mountains.ts
@@ -0,0 +1,35 @@
+export interface Mountain {
+  name: string;
+  height: number;
+}
+
+let mountains: Mountain[] = [
+  { name: 'Kilmanjaro', height: 19341 },
+  { name: 'Everest', height: 29029 },
+  { name: 'Denali', height: 20310 },
+];
+
+export function findNameOfTallestMountain(array: Mountain[]): string {
+  let highest: number | null = null;
+  let highestName: string = '';
+  for (let mountain of array) {
+    if (highest === null) {
+      highest = mountain.height;
+      highestName = mountain.name;
+    } else if (mountain.height > highest) {
+      highest = mountain.height;
+      highestName = mountain.name;
+    }
+  }
+  return highestName;
+}
+console.log(findNameOfTallestMountain(mountains));
+// {
+
+// for (let hill of array){
+//   console.log(hill);
+//   if (hill[0].height > hill[1].height ){
+//    if(hill[0].height >hill[2].height)
+//   {
+//   return hill[0].name;
+//   }
